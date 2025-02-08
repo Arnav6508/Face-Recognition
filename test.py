61ea8bb36e3e9374850781e429793a58e6be800a
Arnav6508 <arnavgupta6508@gmail.com>
2025-02-08 16:04:52 +0530

    added test_from_webcam function

--- test.py
+++ test.py
@@ -1,49 +1,50 @@
-import numpy as np
 import cv2
-from util import get_embeddings
-import sqlite3
+from utils import get_embeddings, find_best_match
+from ultralytics import YOLO
 
-def cosine_similarity(embedding1, embedding2):
-    embedding1 = embedding1.flatten()
-    embedding2 = embedding2.flatten()
+model_path = 'best.pt'
+embedding_db_path = 'test.db'
 
-    norm1 = np.linalg.norm(embedding1)
-    norm2 = np.linalg.norm(embedding2)
-    if norm1 == 0 or norm2 == 0: return 0.0
-    
-    similarity = np.dot(embedding1, embedding2) / (norm1 * norm2)
-    return similarity
-
-def find_best_match(test_embedding, embedding_db_path):
-    conn = sqlite3.connect(embedding_db_path)
-    cursor = conn.cursor()
-
-    best_match_name = None
-    best_similarity = -1  
-
-    cursor.execute('SELECT * FROM Embeddings')
-    embeddings_db = cursor.fetchall()
-
-    for entry in embeddings_db:
-
-        name, embedding_blob = entry
-        stored_embedding = np.frombuffer(embedding_blob, dtype=np.float32)
-        similarity = cosine_similarity(test_embedding, stored_embedding)  
-
-        if similarity > best_similarity:  
-            best_similarity = similarity
-            best_match_name = name
-    
-    print(best_match_name)
-    return best_match_name
+model = YOLO(model_path)
 
 def test_from_path(image_path, embedding_db_path):
     image = cv2.imread(image_path)
-    embedding = get_embeddings(image)
-    if embedding is None: return None
-    return find_best_match(embedding, embedding_db_path)
+    return test_from_image(image, embedding_db_path)
 
 def test_from_image(image, embedding_db_path):
     embedding = get_embeddings(image)
     if embedding is None: return None
     return find_best_match(embedding, embedding_db_path)
+
+def test_from_webcam(embedding_db_path = embedding_db_path, threshold = 0.5):
+    cap = cv2.VideoCapture(0)
+
+    ret, frame = cap.read()
+    while ret == True:
+
+        results = model(frame)[0]
+
+        for result in results.boxes.data.tolist():
+            x1, y1, x2, y2, score, class_id = result
+            cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 4)
+
+            if score > threshold:
+
+                cropped_image = frame[int(y1): int(y2), int(x1): int(x2)]
+                label = test_from_image(cropped_image, embedding_db_path)
+
+                if label != None:
+                    cv2.putText(frame, label, (int(x1), int(y1 - 10)),
+                                cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
+                
+                else:
+                    cv2.putText(frame, 'Unknown', (int(x1), int(y1 - 10)),
+                                cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA)
+
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'): break
+
+        ret, frame = cap.read()
+        
+    cap.release()
+    cv2.destroyAllWindows()
